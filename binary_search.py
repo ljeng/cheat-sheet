45a26c68397bf3f97dc41987c7a5ada3539b33ca
Lareine Jeng <23950069+ljeng@users.noreply.github.com>
2020-03-22 06:02:21 -0400

    Create binary_search.py
    Rewrite binary_search.md

---
+++ binary_search.py
@@ -0,0 +1,14 @@
+import bisect
+
+
+class A:
+
+    def __init__(self, function):
+        self.function = function
+
+    def __getitem__(self, k):
+        return self.function(k)
+
+
+def binary_search(function, start, end):
+    return bisect.bisect_left(A(function), True, start, end)
